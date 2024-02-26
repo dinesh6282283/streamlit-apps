e8d11071d0c700aa585b71c9aa01666ebe80de2e
21ada17 <141546671+dinesh6282283@users.noreply.github.com>
2024-02-26 09:42:38 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from textblob import TextBlob
+from transformers import pipeline
+from streamlit_extras.let_it_rain import rain
+st.title("Sentiment Analysis App")
+p = pipeline(model = "lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+
+
+message=st.text_area("Please Enter your text")
+if st.button("Analyze the Sentiment"):
+    mod = p(message)
+    result = mod[0]['label'].upper()
+    if result == "POSITIVE":
+        st.warning("The predicted sentiment is positive!!")
+    elif result=="NEUTRAL":
+        st.warning("The predicted sentiment is neutral")
+    else:
+        st.warning("The predicted sentiment is negative")
+    st.success(result)
