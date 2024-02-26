742eee858c482d48a6bd215a75b5e10bed79021e
21ada17 <141546671+dinesh6282283@users.noreply.github.com>
2024-02-26 15:07:09 +0530

    Update machine_translation.py

--- machine_translation.py
+++ machine_translation.py
@@ -1,8 +1,11 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+import streamlit as st
+st.title("English to Tamil Translator")
 model_name="jbochi/madlad400-3b-mt"
 model=T5ForConditionalGeneration.from_pretrained(model_name,device_map='auto')
 tokenizer=T5Tokenizer.from_pretrained(model_name)
-text="<2pt> I love pizza!"
+text="<2ta> "+st.text_input("Enter your text")
 input_ids=tokenizer(text,return_tensors='pt').input_ids.to(model.device)
 outputs=model.generate(input_ids=input_ids)
-tokenizer.decode(outputs[0],skip_special_tokens=True)
+op=tokenizer.decode(outputs[0],skip_special_tokens=True)
+st.write(op)
